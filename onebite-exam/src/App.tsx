b6cc510a2321a9de8eaaa540d97d4dff09441d43
이범진 <bumttang01@naver.com>
2025-10-23 18:00:29 +0900

    Shadcn/ui 예제

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -1,19 +1,123 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { cn } from "./lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Toaster } from "@/components/ui/sonner";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { ChefHat } from "lucide-react";
 
 function App() {
-  const isActive = true;
-  return (
-    <div>
-      <Button>버튼!</Button>
-      {/* 여러 조건부 스타일을 표현하려면 cn 함수가 좋다? */}
-      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
-        isActive
-      </div>
+  const isActive = false;
 
-      {/* 단순 조건이면 그냥 삼항 연산자로도 가능(혹은 if else) */}
-      <div className={isActive ? "text-green-500" : "text-red-500"}>
+  return (
+    <div className="p-5">
+      {/* svg 형식, 이미지 import 가능 */}
+      <ChefHat className="h-10 w-10 fill-red-500" />
+      <AlertDialog>
+        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Title</AlertDialogTitle>
+          <AlertDialogDescription>Description</AlertDialogDescription>
+          <div>body</div>
+          <div>
+            <AlertDialogAction
+              onClick={() => {
+                console.log("Action!");
+              }}
+            >
+              Action
+            </AlertDialogAction>
+            <AlertDialogCancel onClick={() => console.log("Cancel")}>
+              Cancel
+            </AlertDialogCancel>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Title</DialogTitle>
+          <div>Body</div>
+        </DialogContent>
+      </Dialog>
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>Open</Button>
+        </PopoverTrigger>
+        <PopoverContent>Content!</PopoverContent>
+      </Popover>
+
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">1</CarouselItem>
+          <CarouselItem className="basis-1/3">2</CarouselItem>
+          <CarouselItem className="basis-1/3">3</CarouselItem>
+          <CarouselItem className="basis-1/3">4</CarouselItem>
+          <CarouselItem className="basis-1/3">5</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <Toaster />
+
+      <Textarea />
+
+      <Input placeholder="입력 ..." />
+
+      <Button
+        onClick={() => {
+          toast("토스트 메세지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <Button variant={"destructive"}>버튼</Button>
+      <Button variant={"ghost"}>버튼</Button>
+      <Button variant={"link"}>버튼</Button>
+      <Button variant={"outline"}>버튼</Button>
+      <Button variant={"secondary"}>버튼</Button>
+
+      <div
+        className={cn(
+          "w-10 text-sm",
+          isActive ? "text-green-500" : "text-red-500"
+        )}
+      >
         isActive
       </div>
 
