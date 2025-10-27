b4750ad0da066ffc2a61ae874ebec33c7d46cff4
이범진 <bumttang01@naver.com>
2025-10-27 13:47:50 +0900

    zustand 미들웨어(immer, combine) 사용법 2

--- onebite-exam/src/store/count.ts
+++ onebite-exam/src/store/count.ts
@@ -1,6 +1,12 @@
 // state, action 함수를 포함하는 객체인 store 생성
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 // create(() => {
@@ -20,38 +26,67 @@ import { immer } from "zustand/middleware/immer";
 // };
 
 export const useCountStore = create(
-  // immer
-  // -> 상태를 직접 바꾸는 것처럼 써도, 내부적으로 안전하게 불변 상태를 유지시켜주는 미들웨어
-  immer(
-    // combine
-    // -> 현재 store의 type을 첫번째 인자를 통해 자동으로 추론
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          // const count = get().count;
-          // set({
-          //   count: count + 1,
-          // });
+  devtools(
+    // persist
+    // -> browser local storage 에 저장
+    persist(
+      // subscribeWithSelector
+      // -> store 값 중 특정 부분만 감시
+      subscribeWithSelector(
+        // immer
+        // -> 상태를 직접 바꾸는 것처럼 써도, 내부적으로 안전하게 불변 상태를 유지시켜주는 미들웨어
+        immer(
+          // combine
+          // -> 현재 store의 type을 첫번째 인자를 통해 자동으로 추론
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                // const count = get().count;
+                // set({
+                //   count: count + 1,
+                // });
 
-          // set((state) => ({
-          //   count: state.count + 1,
-          // }));
+                // set((state) => ({
+                //   count: state.count + 1,
+                // }));
 
-          set((state) => {
-            state.count += 1;
-          });
-        },
-        decrease: () => {
-          // set((state) => ({
-          //   count: state.count - 1,
-          // }));
-          set((state) => {
-            state.count -= 1;
-          });
-        },
-      },
-    }))
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                // set((state) => ({
+                //   count: state.count - 1,
+                // }));
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          }))
+        )
+      ),
+      {
+        name: "countStore",
+        // partialize -> store 중에 어떤 값만 저장할지 고르는 함수
+        partialize: (store) => ({ count: store.count }),
+        // createJSONStorage -> 보관할 storage 지정
+        storage: createJSONStorage(() => sessionStorage),
+      }
+    ),
+    { name: "countStore" }
   )
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listner
+    console.log("count: ", count, " || prevCount:", prevCount);
+
+    const store = useCountStore.getState();
+    // useCountStore.setState((store)=>({ }))
+  }
 );
 
 // export const useCountStore = create<Store>((set, get) => ({
