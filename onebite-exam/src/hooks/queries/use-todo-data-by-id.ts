6f1eac8ea250f9da56f30015bc91b3f867865101
이범진 <bumttang01@naver.com>
2025-10-27 17:05:55 +0900

    tanstack query - 캐싱 매커니즘 이해하기 2

--- onebite-exam/src/hooks/queries/use-todo-data-by-id.ts
+++ onebite-exam/src/hooks/queries/use-todo-data-by-id.ts
@@ -12,5 +12,7 @@ export function useTodoDataById(id: number) {
     // refetchOnMount: false, // 컴포넌트가 마운트될 때마다 자동으로 refetch할지 여부
     // refetchOnWindowFocus: false, // 사용자가 브라우저 탭을 다시 포커스할 때 refetch할지 여부
     // refetchOnReconnect: false, // 네트워크 연결이 끊겼다가 다시 연결될 때 refetch할지 여부
+
+    gcTime: 5000, // TanStack Query에서 캐시된 데이터가 더 이상 사용되지 않을 때 메모리에서 제거
   });
 }
--- onebite-exam/src/main.tsx
+++ onebite-exam/src/main.tsx
@@ -7,7 +7,21 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // server state와 관련된 store라 생각하면 됨?
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  // main.tsx 에서 설정하는 값은, 전역으로 옵션 설정(default 설정)
+  // 각 쿼리에서 사용하는 옵션이 우선 적용
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      gcTime: 5 * 60 * 1000,
+
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
