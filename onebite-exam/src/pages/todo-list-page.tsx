9e84426d1bd6b38641eefb8659ae08e5ada1e7d4
이범진 <bumttang01@naver.com>
2025-10-27 14:52:51 +0900

    zustand를 이용한 TodoList 기능 구현

--- onebite-exam/src/pages/todo-list-page.tsx
+++ onebite-exam/src/pages/todo-list-page.tsx
@@ -1,7 +1,8 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
+import { useTodos } from "@/store/todos";
 
-const dummyTodos = [
+/* const dummyTodos = [
   {
     id: 1,
     content: "Todo 1",
@@ -14,14 +15,15 @@ const dummyTodos = [
     id: 3,
     content: "Todo 3",
   },
-];
+]; */
 
 export default function TodoListPage() {
+  const todos = useTodos();
   return (
     <div className="p-5 flex flex-col gap-5">
       <h1 className="text-2xl font-bold">TodoList</h1>
       <TodoEditor />
-      {dummyTodos.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
