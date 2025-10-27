6060bc359d11c79530e657369a1b98a216e16fe9
이범진 <bumttang01@naver.com>
2025-10-27 13:22:04 +0900

    zustand 미들웨어(immer, combine) 사용법

--- onebite-exam/src/store/count.ts
+++ onebite-exam/src/store/count.ts
@@ -1,5 +1,7 @@
 // state, action 함수를 포함하는 객체인 store 생성
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 // create(() => {
 //   return {
@@ -9,34 +11,69 @@ import { create } from "zustand";
 //   };
 // });
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+// type Store = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      // const count = get().count;
-      // set({
-      //   count: count + 1,
-      // });
+export const useCountStore = create(
+  // immer
+  // -> 상태를 직접 바꾸는 것처럼 써도, 내부적으로 안전하게 불변 상태를 유지시켜주는 미들웨어
+  immer(
+    // combine
+    // -> 현재 store의 type을 첫번째 인자를 통해 자동으로 추론
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          // const count = get().count;
+          // set({
+          //   count: count + 1,
+          // });
 
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+          // set((state) => ({
+          //   count: state.count + 1,
+          // }));
+
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          // set((state) => ({
+          //   count: state.count - 1,
+          // }));
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    }))
+  )
+);
+
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       // const count = get().count;
+//       // set({
+//       //   count: count + 1,
+//       // });
+
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
