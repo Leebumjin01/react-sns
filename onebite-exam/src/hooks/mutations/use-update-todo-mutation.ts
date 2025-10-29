53e4d992a015af87dcd8bd8a18ad11d7a816c766
이범진 <bumttang01@naver.com>
2025-10-29 17:58:19 +0900

    캐시 데이터 다루기 3 - 낙관적 업데이트 예외처리

--- onebite-exam/src/hooks/mutations/use-update-todo-mutation.ts
+++ onebite-exam/src/hooks/mutations/use-update-todo-mutation.ts
@@ -7,7 +7,14 @@ export function useUpdateTodoMutation() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: updateTodo,
-    onMutate: (updatedTodo) => {
+    // 낙관적 업데이트
+    onMutate: async (updatedTodo) => {
+      // 낙관적 업데이트 진행 중에 데이터 조회 요청이 들어오면 취소
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list); // 업데이트 이전 todos data
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
         if (!prevTodos) return [];
         return prevTodos.map((prevTodo) =>
@@ -15,6 +22,25 @@ export function useUpdateTodoMutation() {
             ? { ...prevTodo, ...updatedTodo }
             : prevTodo
         );
+      });
+
+      return {
+        prevTodos,
+      };
+    },
+    // 요청 실패시 낙관적 업데이트 취소
+    onError: (error, variable, context) => {
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos
+        );
+      }
+    },
+    // 데이터 수정 요청 마무리시 todolist 캐시 데이터를 무효화 후 서버로부터 불러온 값으로 갱신
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
       });
     },
   });
