2e9f62a88f0282186ddfbf0a461987876b66a845
이범진 <bumttang01@naver.com>
2025-11-11 17:32:56 +0900

    캐싱 설정 - 캐시 조작

--- onebite-log/src/hooks/mutations/post/use-create-post.ts
+++ onebite-log/src/hooks/mutations/post/use-create-post.ts
@@ -1,12 +1,24 @@
 import { createPostWithImages } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createPostWithImages,
     onSuccess: () => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      // 방법 1. 캐시 초기화
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
+      // 방법 2. 캐시 데이터에 완성된 포스트만 추가
+
+      // 방법 3. 낙관적 업데이트 방식(onMutate)
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
